653cf8493230f9e0c13909a0dad0ae96a0e65674
Jin Ooi <jinooinyc@gmail.com>
2017-07-19 14:34:52 -0400

    Finish association exercise

---
+++ Association_Exercise/app/models/enrollment.rb
@@ -0,0 +1,16 @@
+class Enrollment < ActiveRecord::Base
+  belongs_to(
+    :course,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Course
+    )
+
+    belongs_to(
+    :student,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :User
+    )
+
+end
